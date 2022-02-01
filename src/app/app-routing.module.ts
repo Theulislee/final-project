664e7ec7f292fa862ade0aae3f1f15688327c804
Matheus Magalhães <magalhaescerqueiraesilva@gmail.com>
2022-02-01 16:49:06 -0300

    Created the routes from the module Gerenciador de tarefas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardRoutes } from './dashboard/dashboard-routing.module'; //Add dashboard-routing
 import { CalculadoraRoutes } from './calculadora/calculadora-routing.module'; //Add calculadora-routing
 import { ConversorRoutes } from './conversor/conversor-routing.module'; //Add conversor-routing
+import { TarefaRoutes } from './tarefas';
 
 export const routes: Routes = [ //Minhas rota que irá fazer o redirect para dashboard.
    { //Ao clicar no path vazio iremos ser redirecionado ao dashboard,calculadora,conversor ...
@@ -13,7 +14,8 @@ export const routes: Routes = [ //Minhas rota que irá fazer o redirect para das
   },
   ...DashboardRoutes,
 	...CalculadoraRoutes,
-  ...ConversorRoutes
+  ...ConversorRoutes,
+  ...TarefaRoutes
 ];
 
 /**
